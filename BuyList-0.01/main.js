378b79f8eef35aac624914ac0f92a8134e4e9161
Kozopas Viktoria <kozopasv@gmail.com>
2017-10-05 19:51:28 +0300

    я щось робила, але нічого не працює
    таке життя

--- BuyList-0.01/main.js
+++ BuyList-0.01/main.js
@@ -2,11 +2,22 @@ $(function () {
     var LIST = $(".list-of-products");
     var ITEM_TEMPLATE = $(".one-row-template").html();
 
+    var RIGHT_LIST = $(".bl-right-prod-list-bought");
+    var RIGHT_ITEM_TEMPLATE = $(".one-product-template").html();
+
     function addItem(title) {
         var $node = $(ITEM_TEMPLATE);
+
+        var $right_node = $(RIGHT_ITEM_TEMPLATE);
+
         var quantity = 1;
         var $quantity_label = $node.find(".bl-quantity-label");
         $quantity_label.text(quantity);
+
+        //when start
+        $node.find(".bl-minus").css('opacity', '0.6');
+        $node.find(".bl-minus").prop("disabled", true);
+        //that was bad code
 
         $node.find(".bl-product-name").text(title);
 
@@ -14,10 +25,15 @@ $(function () {
             if (quantity > 1) {
                 quantity -= 1;
                 $quantity_label.text(quantity);
+            }else if (quantity == 1){
+                $node.find(".bl-minus").prop("disabled", true);
+                $node.find(".bl-minus").css('opacity', '0.6');
             }
         });
 
         $node.find(".bl-plus").click(function () {
+            $node.find(".bl-minus").prop("disabled", false);
+            $node.find(".bl-minus").css('opacity', '1');
             quantity += 1;
             $quantity_label.text(quantity);
         });
@@ -26,27 +42,44 @@ $(function () {
             $node.remove();
         });
 
+        //Bought product НЕ АБОТАЕТ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        $node.find(".bl-bought").click(function () {
+            // $node.remove();
+            alert("sth");
+            $node.addClass(".is-bought");
+        });
+
+        //unboought product
+        //НЕ АБОТАЕТ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        $node.find(".bl-unbought").click(function () {
+            $node.removeClass("is-bought");
+        });
+
+
+        //і тут НЕ АБОТАЕТ - редагує але не повертається попередня форма
         $node.find(".bl-product-name").click(function () {
             $node.find(".bl-product-name").hide();
-            $node.find(".edit").show();
-            $node.find(".edit").val(title);
-            $node.find(".bl-product-name").text(title);
+            $node.find(".edit-name").show();
+            $node.find(".edit-name").val(title);
         });
 
         $node.find(".bl-product-name").focusout(function () {
             $node.find(".bl-product-name").show();
-            $node.find(".edit").hide();
-            title = $node.find(".edit").val();
+            $node.find(".edit-name").hide();
+            title = $node.find(".edit-name").val();
+            $node.find(".bl-product-name").text(title);
 
         });
 
         LIST.append($node);
+        RIGHT_LIST.append($right_node);
     }
 
     addItem("Помідори");
     addItem("Печиво");
     addItem("Сир");
 
+    //add new product
     var $new_input = $(".input-text");
     $(".button-add").click(function () {
         var new_name = $new_input.val();
@@ -56,7 +89,7 @@ $(function () {
             $new_input.focus();
         }
     });
-
+    //add new product enter pressed
     $(".input-text").keyup(function (e) {
         if (e.which == 13) {
             var new_name = $new_input.val();
