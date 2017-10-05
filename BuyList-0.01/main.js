3e1f75116b7f7d87d52b766fd7f9fa381ee30425
Kozopas Viktoria <kozopasv@gmail.com>
2017-10-05 16:54:03 +0300

    може це щось закомітити?
    може.
    я щось зробила.
    воно seems to be working, working hard.

--- BuyList-0.01/main.js
+++ BuyList-0.01/main.js
@@ -2,18 +2,69 @@ $(function () {
     var LIST = $(".list-of-products");
     var ITEM_TEMPLATE = $(".one-row-template").html();
 
-    function addItem(title){
-        var $node  = $(ITEM_TEMPLATE);
-        var quantity = 3;
+    function addItem(title) {
+        var $node = $(ITEM_TEMPLATE);
+        var quantity = 1;
         var $quantity_label = $node.find(".bl-quantity-label");
         $quantity_label.text(quantity);
 
         $node.find(".bl-product-name").text(title);
 
+        $node.find(".bl-minus").click(function () {
+            if (quantity > 1) {
+                quantity -= 1;
+                $quantity_label.text(quantity);
+            }
+        });
 
+        $node.find(".bl-plus").click(function () {
+            quantity += 1;
+            $quantity_label.text(quantity);
+        });
+
+        $node.find(".bl-del").click(function () {
+            $node.remove();
+        });
+
+        $node.find(".bl-product-name").click(function () {
+            $node.find(".bl-product-name").hide();
+            $node.find(".edit").show();
+            $node.find(".edit").val(title);
+            $node.find(".bl-product-name").text(title);
+        });
+
+        $node.find(".bl-product-name").focusout(function () {
+            $node.find(".bl-product-name").show();
+            $node.find(".edit").hide();
+            title = $node.find(".edit").val();
+
+        });
 
         LIST.append($node);
     }
 
-    addItem("something");
+    addItem("Помідори");
+    addItem("Печиво");
+    addItem("Сир");
+
+    var $new_input = $(".input-text");
+    $(".button-add").click(function () {
+        var new_name = $new_input.val();
+        if (new_name.trim()) {
+            addItem(new_name);
+            $new_input.val("");
+            $new_input.focus();
+        }
+    });
+
+    $(".input-text").keyup(function (e) {
+        if (e.which == 13) {
+            var new_name = $new_input.val();
+            if (new_name.trim()) {
+                addItem(new_name);
+                $new_input.val("");
+                $new_input.focus();
+            }
+        }
+    });
 });
