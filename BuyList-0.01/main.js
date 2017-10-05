4165908359724795cc05af76b6963e25e3482344
Kozopas Viktoria <kozopasv@gmail.com>
2017-10-05 23:27:48 +0300

    part of right statistic

--- BuyList-0.01/main.js
+++ BuyList-0.01/main.js
@@ -2,29 +2,35 @@ $(function () {
     var LIST = $(".list-of-products");
     var ITEM_TEMPLATE = $(".one-row-template").html();
 
-    var RIGHT_LIST = $(".bl-right-prod-list-bought");
-    var RIGHT_ITEM_TEMPLATE = $(".one-product-template").html();
+    var LEFT_LIST_LEFT = $(".bl-right-prod-list-left");
+    var LEFT_ITEM_TEMPLATE = $(".one-product-template").html();
+
+    var BOUGHT_LIST_LEFT = $(".bl-right-prod-list-bought");
+    var BOUGHT_ITEM_TEMPLATE = $(".one-product-template").html();
 
     function addItem(title) {
         var $node = $(ITEM_TEMPLATE);
-
-        var $right_node = $(RIGHT_ITEM_TEMPLATE);
-
         var quantity = 1;
         var $quantity_label = $node.find(".bl-quantity-label");
         $quantity_label.text(quantity);
+        $node.find(".bl-product-name").text(title);
+
+        var $right_node = $(LEFT_ITEM_TEMPLATE);
+        $right_node.find(".titleProduct").text(title);
+        var $orange_quantity = $right_node.find(".ui-circular-number");
+        $orange_quantity.text(quantity);
 
         //when start
         $node.find(".bl-minus").css('opacity', '0.6');
         $node.find(".bl-minus").prop("disabled", true);
         //that was bad code
 
-        $node.find(".bl-product-name").text(title);
 
         $node.find(".bl-minus").click(function () {
             if (quantity > 1) {
                 quantity -= 1;
                 $quantity_label.text(quantity);
+                $orange_quantity.text(quantity);
             }else if (quantity == 1){
                 $node.find(".bl-minus").prop("disabled", true);
                 $node.find(".bl-minus").css('opacity', '0.6');
@@ -36,6 +42,7 @@ $(function () {
             $node.find(".bl-minus").css('opacity', '1');
             quantity += 1;
             $quantity_label.text(quantity);
+            $orange_quantity.text(quantity);
         });
 
         $node.find(".bl-del").click(function () {
@@ -72,7 +79,7 @@ $(function () {
         });
 
         LIST.append($node);
-        RIGHT_LIST.append($right_node);
+        LEFT_LIST_LEFT.append($right_node);
     }
 
     addItem("Помідори");
